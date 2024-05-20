f89e9ac9e7829aaaac360368949da716abd639c1
Julia <klimuk.julia@gmail.com>
2024-05-20 16:55:55 +0300

    added dz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1 = 21;
+        int num2 = 3;
+        int num3 = 17;
+        int num4 = 1;
+
+        int sum1 = num1 + num2;
+        int sum2 = num1 + num2;
+
+        boolean isSum1Greater1 = sum1 > sum2;
+        System.out.println("Перша сума більша за другу: " + isSum1Greater1);
+
+         sum1 +=  1;
+         sum2 +=  2;
+
+        boolean isSum1Greater2 = sum1 > sum2;
+        System.out.println("Перша сума більша за другу: " + isSum1Greater2);
+
+        boolean isSumEven = (sum1 % 2 == 0) || (sum2 % 2 == 0);
+        System.out.println("Хоча б одна сума кратна 2: " + isSumEven);
     }
 }
